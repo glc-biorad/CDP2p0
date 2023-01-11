47f003edd314adc5c7a432b03dd61dd0fa9ad14a
Gabe Lopez-Candales <107704834+glc-biorad@users.noreply.github.com>
2023-01-10 23:51:28 -0500

    Add files via upload

---
+++ cdp2p0_v1.0.0.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.8
+
+import pythonnet
+from pythonnet import load
+
+try:
+	load("coreclr")
+except:
+	print("Cannot load coreclr into gui")
+
+from gui.controllers.controller import Controller
+from gui.models.model import Model
+from gui.views.view import View
+
+def main() -> None:
+	unit = 'A'
+	print(f"Unit: {unit}")
+	model = Model(unit=unit)
+	view = View(model)
+	controller = Controller(model, view)
+	controller.setup_bindings()
+	controller.run()
+
+if __name__ == '__main__':
+	main()
