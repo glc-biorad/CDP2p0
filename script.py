ba1ca8da820928530b109b76eed06cf2bed622fc
Gabe Lopez-Candales <107704834+glc-biorad@users.noreply.github.com>
2023-03-06 10:10:04 -0500

    Add files via upload

--- script.py
+++ script.py
@@ -1,7 +1,13 @@
 from api.upper_gantry.upper_gantry import UpperGantry
 from api.upper_gantry.seyonic.seyonic import Seyonic
+from api.reader.meerstetter.meerstetter import Meerstetter
+
+import time
 
 if __name__ == '__main__':
     #ug = UpperGantry()
     #ug.get_pipettor().liquid_level_detect()
-    s = Seyonic()
+    m = Meerstetter()
+    t_start = time.time()
+    m.get_temperature(4)
+    print(f"Total Time: {time.time() - t_start}")
