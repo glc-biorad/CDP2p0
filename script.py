fe0ba4f3c97c02e46942f4cf16653bdeeea9cef6
Gabe Lopez-Candales <107704834+glc-biorad@users.noreply.github.com>
2023-03-07 10:47:14 -0500

    Add files via upload

--- script.py
+++ script.py
@@ -7,7 +7,53 @@ import time
 if __name__ == '__main__':
     #ug = UpperGantry()
     #ug.get_pipettor().liquid_level_detect()
+
     m = Meerstetter()
-    t_start = time.time()
-    m.get_temperature(4)
-    print(f"Total Time: {time.time() - t_start}")
+    address = 2
+    cutoff = 3
+    
+    target_temp = 30
+    print(target_temp)
+    m.change_temperature(address, target_temp, True)
+    temp = m.get_temperature(address) 
+    
+    target_temp = 60
+    print(target_temp)
+    m.change_temperature(address, target_temp, True)
+    print(f'Wait 300 seconds')
+    time.sleep(300)
+
+    target_temp = 40
+    print(target_temp)
+    m.change_temperature(address, target_temp, True)
+    print(f'Wait 400 seconds')
+    time.sleep(400)
+
+    target_temp = 50
+    print(target_temp)
+    m.change_temperature(address, target_temp, True)
+    print(f'Wait 300 seconds')
+    time.sleep(300)
+
+    target_temp = 84
+    print(target_temp)
+    m.change_temperature(address, target_temp, True)
+    print(f'Wait 500 seconds')
+    time.sleep(500)
+    
+    
+    target_temp = 4
+    print(target_temp)
+    m.change_temperature(address, target_temp, True)
+    print(f'Wait 400 seconds')
+    time.sleep(400)
+
+    target_temp = 84
+    print(target_temp)
+    m.change_temperature(address, target_temp, True)
+    print(f'Wait 500 seconds')
+    time.sleep(500)
+    
+    target_temp = 30
+    print(target_temp)
+    m.change_temperature(address, target_temp, True)
